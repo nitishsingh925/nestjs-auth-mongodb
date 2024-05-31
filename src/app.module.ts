bfea8a5172af74fdbc02648cafc4755c216ac67a
Nitish Singh <nitishsingh925@gmail.com>
2024-05-31 06:57:20 +0530

    Config env getOrThrow  useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,31 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/config';
 
 @Module({
   imports: [
-    JwtModule.register({
-      global: true,
-      secret: 'secret123',
-      signOptions: { expiresIn: '1h' },
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [config],
     }),
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nestjs-auth'),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+      }),
+      global: true,
+      inject: [ConfigService],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.getOrThrow('MONGO_DB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     AuthModule,
   ],
   controllers: [AppController],
